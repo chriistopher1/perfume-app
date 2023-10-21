3b0de0fae66381029d96fc755dce303d4f2a410a
chriistopher1 <142476784+chriistopher1@users.noreply.github.com>
2023-10-22 01:51:02 +0700

    Update navigation-bar.js

--- src/components/navigation-bar/navigation-bar.js
+++ src/components/navigation-bar/navigation-bar.js
@@ -12,26 +12,18 @@ function NavigationBar() {
       <div className="font-bold p-4">LOGO</div>
       <AiOutlineMenu className="text-2xl md:hidden cursor-pointer text-gray-400 hover:text-white transition duration-300" onClick={() => {setIsOpen(!isOpen)}}/>
       <div className={`navigation-link-container relative md:flex gap-4 ${isOpen ? "left-0" : "left-full md:left-0"}`}>
-        <div className="navigation-link p-1 pl-4 pr-4 max-md:p-4">
-          <a className="font-bold" href="#home">
-            HOME
-          </a>
-        </div>
-        <div className="navigation-link p-1 pl-4 pr-4 max-md:p-4">
-          <a className="font-bold" href="#aboutus">
-            ABOUT US
-          </a>
-        </div>
-        <div className="navigation-link p-1 pl-4 pr-4 max-md:p-4">
-          <a className="font-bold" href="#product">
-            OUR PRODUCT
-          </a>
-        </div>
-        <div className="navigation-link p-1 pl-4 pr-4 max-md:p-4">
-          <a className="font-bold" href="#contact">
-            CONTACT
-          </a>
-        </div>
+        <a className="navigation-link p-1 pl-4 pr-4 max-md:p-4" href="#home">
+          <span className="font-bold">HOME</span>
+        </a>
+        <a className="navigation-link p-1 pl-4 pr-4 max-md:p-4" href="#aboutus">
+          <span className="font-bold">ABOUT US</span>
+        </a>
+        <a className="navigation-link p-1 pl-4 pr-4 max-md:p-4" href="#product">
+          <span className="font-bold">OUR PRODUCT</span>
+        </a>
+        <a className="navigation-link p-1 pl-4 pr-4 max-md:p-4" href="#contact">
+          <span className="font-bold">CONTACT</span>
+        </a>
       </div>
     </div>
   );
