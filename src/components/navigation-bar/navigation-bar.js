b12e695b61294ab1b50e2601916e3815add4c15f
chriistopher1 <142476784+chriistopher1@users.noreply.github.com>
2023-10-25 15:14:16 +0700

    Update navigation-bar.js

--- src/components/navigation-bar/navigation-bar.js
+++ src/components/navigation-bar/navigation-bar.js
@@ -9,26 +9,11 @@ function NavigationBar() {
   
 
   return (
-<<<<<<< HEAD
-    <div className="navbar fixed z-20 flex w-full items-center justify-between bg-[#0F0F0F] pr-8 opacity-90">
-      <div className="p-4 font-bold">LOGO</div>
-      <AiOutlineMenu
-        className="cursor-pointer text-2xl text-gray-400 transition duration-300 hover:text-white md:hidden"
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-      />
-      <div
-        className={`navigation-link-container relative gap-4 md:flex ${
-          isOpen ? "left-0" : "left-full md:left-0"
-        }`}
-      >
-=======
     <div className="navbar flex w-full items-center justify-between bg-[#0F0F0F] pr-8 fixed z-20 opacity-90">
       <div className="font-bold p-4">LOGO</div>
       <AiOutlineMenu className="text-2xl md:hidden cursor-pointer text-gray-400 hover:text-white transition duration-300" onClick={() => {setIsOpen(!isOpen)}}/>
       <div className={`navigation-link-container relative md:flex gap-4 ${isOpen ? "left-0" : "left-full md:left-0"}`}>
->>>>>>> 3b0de0fae66381029d96fc755dce303d4f2a410a
+
         <a className="navigation-link p-1 pl-4 pr-4 max-md:p-4" href="#home">
           <span className="font-bold">HOME</span>
         </a>
@@ -41,15 +26,14 @@ function NavigationBar() {
         <a className="navigation-link p-1 pl-4 pr-4 max-md:p-4" href="#contact">
           <span className="font-bold">CONTACT</span>
         </a>
-<<<<<<< HEAD
         <a className="navigation-link p-1 pl-4 pr-4 max-md:p-4" href="#program">
           <span className="font-bold">PROGRAM</span>
         </a>
         
-=======
->>>>>>> 3b0de0fae66381029d96fc755dce303d4f2a410a
+
       </div>
     </div>
+
   );
 }
 
